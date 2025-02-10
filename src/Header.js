9b8c747c5c0c3f7ffaa5f73b62597204a320f466
carmenddi <carmendarca@gmail.com>
2025-02-10 12:00:31 +0100

    test

--- src/Header.js
+++ src/Header.js
@@ -9,7 +9,7 @@ export default function Header() {
         <section className='description'>
           <h1 className='title'>Little Lemon</h1>
           <h2>Chicago</h2>
-          <h3>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus.</h3>
+          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus.</p>
           <button><a href='./Reservation'>Reserve a table</a></button>
         </section>
         <img className='b-img' src={RestaurantFood} alt='starter' />
